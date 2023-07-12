290b31cb956d358c0119e90f8d77c7d07ed43b41
Fair Lucas <lukitaa.27@gmail.com>
2023-07-12 17:35:21 -0300

    commit 1
    general layout, not complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,6 @@ import { BodyComponent } from './pages/body/body.component';
 import { AuthFormComponent } from './shared/auth-form/auth-form.component';
 import { InfoAnimeComponent } from './shared/info-anime/info-anime.component';
 import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
-import { ServicesComponent } from './api/services/services.component';
-import { ModelComponent } from './api/model/model.component';
 import { Err404Component } from './pages/err404/err404.component';
 
 @NgModule({
@@ -22,8 +20,6 @@ import { Err404Component } from './pages/err404/err404.component';
     AuthFormComponent,
     InfoAnimeComponent,
     NavBarComponent,
-    ServicesComponent,
-    ModelComponent,
     Err404Component
   ],
   imports: [
