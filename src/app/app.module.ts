3f14a5b77ef6b2fffe1d89c486c2b7e109661811
Fair Lucas <lukitaa.27@gmail.com>
2023-07-12 12:19:57 -0300

    init proyect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { FooterComponent } from './pages/footer/footer.component';
+import { BodyComponent } from './pages/body/body.component';
+import { AuthFormComponent } from './shared/auth-form/auth-form.component';
+import { InfoAnimeComponent } from './shared/info-anime/info-anime.component';
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
+import { ServicesComponent } from './api/services/services.component';
+import { ModelComponent } from './api/model/model.component';
+import { Err404Component } from './pages/err404/err404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent,
+    AuthFormComponent,
+    InfoAnimeComponent,
+    NavBarComponent,
+    ServicesComponent,
+    ModelComponent,
+    Err404Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/info-anime/info-anime.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-info-anime',
+  templateUrl: './info-anime.component.html',
+  styleUrls: ['./info-anime.component.css']
+})
+export class InfoAnimeComponent {
+
+}
